f20e5d7a83a4dbff4d3f8902ebe486c5288a761a
Chamomile <dltldms5271@gmail.com>
2023-11-30 15:30:24 +0900

    02

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 // Week 13
-// practice 01 
+// practice 02
 //
 // Created by Sieun Lee on 2023/11/30
 //
@@ -8,30 +8,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//형식 선언
-struct student {
-       int ID ;        //학번 
-       char name[100]; //이름
-       float grade;    //학점
-}; //구조체 틀 완성 
-        
+struct point {
+       int x;
+       int y;
+};
+
 int main(int argc, char *argv[])
-{         
-  //instance 선언 (와플 찍어내는 역할)
-  struct student s1 = {1914021, "sIEUN LEE", 4.2 }; //각각 원하는 값으로 초기화. 콤마로 구분.
-  
-  //다른 값 변경 
-  s1.ID = 1914022;
-  s1.name[0] = 'S';
-  s1.grade = 4.3;
-  
-  //출력 
-  printf("ID : %i\n", s1.ID);
-  printf("name : %s\n", s1.name);
-  printf("grade : %f\n", s1.grade);
-  
-  strcpy(s1.name, "Juyeop");
-  printf("name2 : %s\n"); 
+{    
+     struct point p1, p2;
+     int xdiff, ydiff;
+     double dist;
+     
+     printf("input p1 coordinate (x,y) : ");
+     scanf("%d %d", &p1.x, &p1.y);
+     
+     printf("input p2 coordinate (x,y) : ");
+     scanf("%d %d", &p2.x, &p2.y);
+     
+     //distance
+     xdiff = p2.x - p1.x;
+     ydiff = p2.y - p1.y;
+     dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+     
+     printf("distance : %f\n", dist);
    
   system("PAUSE");	
   return 0;
